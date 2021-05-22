ff7615b6b55e8dfeb0a53d06a9546fbef019a0f0
Felipe <felipedelucanogueira@gmail.com>
2021-05-22 02:02:29 -0300

    Mvc e future finalizado

---
+++ lib/controller.dart
@@ -0,0 +1,13 @@
+import 'home_model.dart';
+
+class ControllerHome{
+
+  HomeModel _model = HomeModel();
+
+  Future<List<int>> get lista => _model.lista;
+
+      void loadLista(){
+        _model.loadLista();
+
+      }
+    }
--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'controller.dart';
+
 class HomeView extends StatefulWidget {
   @override
   _HomeViewState createState() => _HomeViewState();
 }
 
 class _HomeViewState extends State<HomeView> {
+  final controller = ControllerHome();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +17,50 @@ class _HomeViewState extends State<HomeView> {
         child: Column(
           children: [
             Expanded(
+              child: Center(
+                child: FutureBuilder<List<int>>(
+                  future: controller.lista,
+                  builder: (context ,snapshot){
+                    if(snapshot.connectionState != ConnectionState.done){
+                     return CircularProgressIndicator();
+                    }
+                    if(snapshot.hasData){
+                      var array = snapshot.data ?? [];
+                      return ListView.builder(
+                          itemCount: array.length,
+                          itemBuilder: (context,index){
+                        return Center(
+                          child: Container(
+                            margin: EdgeInsets.all(10),
+                            height: 30,
+                            width: 100,
+                            child: Center(
+                              child: Text('$index'),
 
+                            ),
+                            decoration: BoxDecoration(
+                              border:  Border.all(color: Colors.black)
+                            ),
+                          ),
+                        );
+
+                      });
+                    }
+                  return Container();
+                    },
+                ),
+              ),
+
+            ),
+            Container(
+              height: 50,
+              child: ElevatedButton(
+                onPressed: (){
+                  setState(() {
+                    controller.loadLista();
+                  });
+                },
+              child: Text('Load'),),
             )
           ],
         ),
